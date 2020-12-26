de6ec381170a473a9e6a7429b4b9a0cc03c8ea2d
arisf <aris.fanaras@mypinpad.group>
2020-12-26 18:25:15 +0000

    Added Swagger and Swagger ui.

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Please download, unzip and run the api.
+
+On the boot up the in-memory H2 db will load the initial records.
+
+Navigate to http://localhost:8080/docs and test the endpoints.
+
--- src/main/java/paymentcardcost/api/PaymentCardCostApplication.java
+++ src/main/java/paymentcardcost/api/PaymentCardCostApplication.java
@@ -2,7 +2,15 @@ package paymentcardcost.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 import paymentcardcost.api.repositories.PaymentCardCostRepository;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication()
 public class PaymentCardCostApplication {
@@ -15,6 +23,12 @@ public class PaymentCardCostApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentCardCostApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return route(GET("/docs"), req ->
+                ServerResponse.temporaryRedirect(URI.create("swagger-ui.html")).build());
     }
 
 }
